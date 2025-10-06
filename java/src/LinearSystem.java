2ae32e6d33ec05f552f1928a4cbe6b37318b9eb8
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-10-06 10:55:31 +0200

    feat(LinearSystem.java): implemented cramers rule for solving

--- java/src/LinearSystem.java
+++ java/src/LinearSystem.java
@@ -8,7 +8,14 @@ public class LinearSystem {
         this.b = b;
     }
 
-    public Vector solve() {
-        return new Vector();
+    public Vector solveWithCramersRule() {
+        double[] entries = new double[this.b.getSize()];
+        double detA = this.A.determinate();
+        for (int j = 0; j < entries.length; j++) {
+            Matrix Aj = Matrix.replaceColumn(A, b);
+            double detAj = Aj.determinate();
+            entries[j] = detA/detAj;
+        }
+        return new Vector(entries);
     }
 }
