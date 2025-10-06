c611744eac824955456ff76bb4b0558b21fab43d
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-10-06 11:08:09 +0200

    fix(LinearSystem): use of replaceColumn, and fraction

--- java/src/LinearSystem.java
+++ java/src/LinearSystem.java
@@ -12,9 +12,9 @@ public class LinearSystem {
         double[] entries = new double[this.b.getSize()];
         double detA = this.A.determinate();
         for (int j = 0; j < entries.length; j++) {
-            Matrix Aj = Matrix.replaceColumn(A, b);
+            Matrix Aj = Matrix.replaceColumn(A, b, j);
             double detAj = Aj.determinate();
-            entries[j] = detA/detAj;
+            entries[j] = detAj/detA;
         }
         return new Vector(entries);
     }
