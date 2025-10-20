9593be4c82e61696970346eb3bd25f6d37141d32
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-10-20 10:56:02 +0200

    fix(LinearSystem)

--- java/src/LinearSystem.java
+++ java/src/LinearSystem.java
@@ -1,9 +1,13 @@
+import implementations.SquareMatrix;
+import implementations.Vector;
+import utility.LinearAlgebra;
+
 public class LinearSystem {
     
-    private Matrix A;
+    private SquareMatrix A;
     private Vector b;
 
-    public LinearSystem(Matrix A, Vector b) {
+    public LinearSystem(SquareMatrix A, Vector b) {
         this.A = A;
         this.b = b;
     }
@@ -12,7 +16,7 @@ public class LinearSystem {
         double[] entries = new double[this.b.getSize()];
         double detA = this.A.determinate();
         for (int j = 0; j < entries.length; j++) {
-            Matrix Aj = Matrix.replaceColumn(A, b, j);
+            SquareMatrix Aj = LinearAlgebra.replaceColumn(A, b, j);
             double detAj = Aj.determinate();
             entries[j] = detAj/detA;
         }
