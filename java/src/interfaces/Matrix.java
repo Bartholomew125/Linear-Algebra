842e7b559cb9cc524e6474904697a5e8ea827f24
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-10-24 13:54:02 +0200

    docs(Matrix.java)

--- java/src/interfaces/Matrix.java
+++ java/src/interfaces/Matrix.java
@@ -5,9 +5,28 @@ import implementations.Vector;
  * Matrix
  */
 public interface Matrix {
+    /**
+     * Returns the element at row i and column j.
+     */
     public double get(int i, int j);
+
+    /**
+     * Return the number of rows of the matrix.
+     */
     public int getNumRows();
+
+    /**
+     * Return the number of columns of the matrix.
+     */
     public int getNumCols();
+
+    /**
+     * Return column j of the matrix.
+     */
     public Vector getColumn(int j);
+
+    /**
+     * Return all of the columns.
+     */
     public Vector[] getColumns();
 }
